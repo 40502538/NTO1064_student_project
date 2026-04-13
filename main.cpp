923fb8699ca74dad46dfb879d6c3349ba1736e63
40502538 <zkoay01@qub.ac.uk>
2026-04-13 16:01:34 +0100

    getName_getExplanation

--- main.cpp
+++ main.cpp
@@ -1,38 +1,73 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class BooleanOperator {
     public:
-        virtual bool respond(bool a, bool b) { return 0; }
-        virtual bool respond(int a) { return 0; }
+        virtual bool evaluate(bool a, bool b = false)  const = 0; 
+        virtual string getName()        const = 0;  
+        virtual string getExplanation() const = 0;  
+        
+
+        virtual ~BooleanOperator() {}
 };
 
 class AND_Operator : public BooleanOperator {
+    public: 
+        string getName()              const override {return "AND";}
+        string getExplanation()       const override {
+            return "AND : True only if both inputs are true.";
+        }
     private:
-        bool respond(bool a, bool b) override { return a && b; }
+        bool evaluate(bool a, bool b) const override { return a && b; }
 };
 
 class OR_Operator : public BooleanOperator {
+    public: 
+        string getName()              const override {return "OR";}
+        string getExplanation()       const override {
+            return "OR : True if at least one input is true.";
+        }
     private:
-        bool respond(bool a, bool b) override { return a || b; }
+        bool evaluate(bool a, bool b) const override { return a || b; }
 };
 
 class NOT_Operator : public BooleanOperator {
+    public: 
+        string getName()              const override {return "NOT";}
+        string getExplanation()       const override {
+            return "NOT : Inverts the input.";
+        }
     private:
-        bool respond(int a) override { return !a; }
+        bool evaluate(bool a, bool) const override { return !a; }
 };
 
 class NAND_Operator : public BooleanOperator {
+    public: 
+        string getName()              const override {return "NAND";}
+        string getExplanation()       const override {
+            return "NAND : Opposite of AND. False only if all inputs are true.";
+        }
     private:
-        bool respond(bool a, bool b) override { return !(a && b); }
+        bool evaluate(bool a, bool b) const override { return !(a && b); }
 };
 
 class NOR_Operator : public BooleanOperator {
+    public: 
+        string getName()              const override {return "NOR";}
+        string getExplanation()       const override {
+            return "NOR : Opposite of OR. True only if all inputs are false.";
+        }
     private:
-        bool respond(bool a, bool b) override { return !(a || b); }
+        bool evaluate(bool a, bool b) const override { return !(a || b); }
 };
 
 class XOR_Operator : public BooleanOperator {
+    public: 
+        string getName()              const override {return "XOR";}
+        string getExplanation()       const override {
+            return "XOR : True if two inputs are different.";
+        }
     private:
-        bool respond(bool a, bool b) override { return a != b; }
+        bool evaluate(bool a, bool b) const override { return a != b; }
 };
